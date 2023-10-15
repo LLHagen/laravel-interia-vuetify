ab948c1ef6a3d1a0245943cd8e5ee6f70ae6d0d4
llhagen <moguntruba@gmail.com>
2023-10-16 07:20:31 +0800

    vuetify install

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/events', function () {
+    return Inertia::render('Events');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
